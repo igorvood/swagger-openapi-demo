067b0c3548009c2b2471716292c001b2bd488e80
vood <balitsky_igor@list.ru>
2021-11-05 17:18:52 +0300

    1

--- src/main/kotlin/ru/vood/swagger/controller/BookController.kt
+++ src/main/kotlin/ru/vood/swagger/controller/BookController.kt
@@ -1,5 +1,12 @@
 package ru.vood.swagger.controller
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
@@ -8,12 +15,27 @@ import ru.vood.swagger.controller.BookNotFoundException
 
 @RestController
 @RequestMapping("/api/book")
-class BookController {
-    @Autowired
-    private val repository: BookRepository? = null
+@Tag(name = "Book", description = "The User API")
+class BookController(private val repository: BookRepository) {
+
+    @ApiResponses(
+        value = [ApiResponse(
+            responseCode = "200",
+            description = "Found the book",
+            content = [Content(
+                mediaType = "application/json",
+                array = ArraySchema(
+                    schema = Schema(
+                        implementation = Book::class
+                    )
+                )
+            )]
+        )]
+    )
+    @Operation(summary = "Gets book by id", tags = ["book"])
     @GetMapping("/{id}")
     fun findById(@PathVariable id: Long): Book {
-        return repository!!.findById(id)
+        return repository.findById(id)
             .orElseThrow { BookNotFoundException() }
     }
 
